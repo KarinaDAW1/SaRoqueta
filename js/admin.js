74272cd42f797acc833df1e65885d58ee68fa34c
KarinaDAW1 <kizquierdo17331@iesjoanramis.org>
2025-06-05 19:32:45 +0200

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -4,7 +4,7 @@ import { BASE_URL } from "./config.js";
 // ./js/admin.js
 
 document.addEventListener('DOMContentLoaded', () => {
-    const loggedInUser = JSON.parse(sessionStorage.getItem('loggedInUser'));
+    const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
 
     if (!loggedInUser || loggedInUser.role !== 'admin') {
         window.location.href = BASE_URL + 'inicio.html';
