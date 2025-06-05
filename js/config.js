519de51aa7fd5d9de75ae283a0d6e0e7651c1d8f
KarinaDAW1 <kizquierdo17331@iesjoanramis.org>
2025-06-06 00:12:24 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,8 @@
-export const BASE_URL = window.location.hostname === "127.0.0.1" ? "" : "/SaRoqueta/";
+const repoName = "SaRoqueta";
+
+export const BASE_URL =
+  window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost"
+    ? "/"
+    : `/${repoName}/`;
+
 console.log("config.js cargado correctamente, BASE_URL:", BASE_URL);
