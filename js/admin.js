f87209a036f00a90df8a8f70ffd685d1d4b230f2
KarinaDAW1 <kizquierdo17331@iesjoanramis.org>
2025-06-06 20:06:13 +0200

    Update admin.js
    Comentarios.

--- js/admin.js
+++ js/admin.js
@@ -5,7 +5,7 @@ import { BASE_URL } from "./config.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
-
+    // Básicamente esto hace que si no estás logeado o tu rol no es 'admin', te envía directamente al inicio.html impidiendote entrar en el admin.html
     if (!loggedInUser || loggedInUser.role !== 'admin') {
         window.location.href = BASE_URL + 'inicio.html';
     }
